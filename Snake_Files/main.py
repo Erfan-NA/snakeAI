456e863080f1b3242dc2d7b943dd82e9e427ea8a
EN-PC <erfan4na@gmial.com>
2024-05-06 18:42:17 -0400

    setup

---
+++ Snake_Files/main.py
@@ -0,0 +1,9 @@
+# This is the beginning of one hell of a grueling project I hope. May 6, 2024.
+# Erfan Nazarian
+
+import snake_classes
+
+MAIN_UP = snake_classes.Direction(0, 1)
+GLOBAL_DOWN = snake_classes.Direction(0, -1)
+GLOBAL_LEFT = snake_classes.Direction(-1, 0)
+GLOBAL_RIGHT = snake_classes.Direction(1, 0)
